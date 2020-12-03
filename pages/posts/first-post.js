ded4ff774c016e68cf50f6ca43b6a4392350d46b
manattan <mogimogi0529@eis.hokudai.ac.jp>
2020-12-03 10:59:52 +0900

    apply style & prettier

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,17 +1,24 @@
-import Link from 'next/link'
+import Head from "next/head";
+import Link from "next/link";
+import Layout from "../../components/Layout";
 
 const FirstPost = () => {
-    // return <h1>Hello, world</h1>
-    return (
-        <div>
-            <h1>Hello, World</h1>
-            <h2>
-                <Link href="/">
-                    <a>Back to Home</a>
-                </Link>
-            </h2>
-        </div>
-    )
-}
+  // return <h1>Hello, world</h1>
+  return (
+    <Layout>
+      <div>
+        <Head>
+          <title>First Post</title>
+        </Head>
+        <h1>Hello, World</h1>
+        <h2>
+          <Link href="/">
+            <a>Back to Home</a>
+          </Link>
+        </h2>
+      </div>
+    </Layout>
+  );
+};
 
-export default FirstPost
+export default FirstPost;
